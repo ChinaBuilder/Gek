817141d560e4f5a9bb00e14076b2984c0f85eec5
ChinaBuilder <wuyunlonggo@gmail.com>
2016-11-28 14:21:41 +0800

    测试用户12

--- src/test/java/com/gek/mapper/UserMapperTestCase.java
+++ src/test/java/com/gek/mapper/UserMapperTestCase.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- *
+ *spring是
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
